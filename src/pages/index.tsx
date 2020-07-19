ca9eaa8edcef711b18aca66f4d8e1be90a8a84f8
IFletcher <Isiahfletcher7@gmail.com>
2020-07-19 14:25:19 -0600

    removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+
 import Layout from '../components/layout';
 import Hero from '../components/hero';
-import Services from '../components/services';
-import Jobs from '../components/jobs';
-import Blogs from '../components/writtenContent';
 
 export default () => {
     return (
