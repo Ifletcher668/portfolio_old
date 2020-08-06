24a1b2dd6766a7dbafc27cb78fb12523e8401fd3
IFletcher <Isiahfletcher7@gmail.com>
2020-08-06 08:07:51 -0600

    updating styles

---
+++ src/components/back_button/backButton.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <button onClick={() => window.history.back()}>Return</button>;
--- src/templates/poem-template.tsx
+++ src/templates/poem-template.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout/layout';
-import { Title } from '../components/titles/titles';
-
-export default ({ data }: { [key: string]: any }) => {
-    const {
-        poem: { title, body, author, published, tags },
-    }: { [key: string]: Poem } = data;
-
-    console.log(body);
-
-    return (
-        <Layout>
-            <Title title={title} />
-            {/* <div>{body}</div> */}
-        </Layout>
-    );
-};
+import { Title, SubTitle } from '../components/titles/titles';
 
 export const query = graphql`
     query GetSinglePoem($slug: String) {
         poem: strapiPoems(slug: { eq: $slug }) {
             title
             body
+            test_body
             author
             published
             tags {
@@ -31,3 +17,39 @@ export const query = graphql`
         }
     }
 `;
+
+export default ({ data }: { [key: string]: any }) => {
+    const {
+        poem: { title, test_body, body, author, published, tags },
+    }: { [key: string]: Poem } = data;
+
+    console.log(test_body);
+
+    return (
+        <Layout>
+            <Title title={title} />
+            <div className='poem'>
+                <article
+                    className='align-left'
+                    dangerouslySetInnerHTML={{ __html: test_body }}></article>
+            </div>
+
+            <hr />
+            <div>
+                <h3>
+                    By {author}, {published}
+                </h3>
+            </div>
+            <div className='tags'>
+                <SubTitle title='tags' />
+                {tags.map(({ tag }, idx) => {
+                    return (
+                        <React.Fragment key={idx}>
+                            <span className='tag'>{tag} </span>
+                        </React.Fragment>
+                    );
+                })}
+            </div>
+        </Layout>
+    );
+};
