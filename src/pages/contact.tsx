dbf3289914cd58e134f706172243e35875b00318
IFletcher <Isiahfletcher7@gmail.com>
2020-09-06 12:31:25 -0600

    including more scss features

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,44 +1,46 @@
-import React from "react";
-import Layout from "../components/layout/layout";
-import { Title } from "../components/titles/titles";
-import SEO from "../components/SEO/SEO";
+import React from 'react';
+import Layout from '../components/layout/layout';
+import { Header } from '../components/titles/titles';
+import SEO from '../components/SEO/SEO';
 
 const contact: React.FC<IProps> = (props: IProps) => {
     return (
         <Layout>
-            <SEO title="Get in Touch" descFor="contactPage" />
-            <section className="contact-page">
-                <article className="contact-form">
-                    <Title title="Get in Touch" />
-                    <form>
-                        <div className="form-group card no-hover no-placement">
-                            <label htmlFor="name">Name: </label>
-                            <input
-                                type="text"
-                                name="name"
-                                // placeholder='name'
-                                className="form-control"
-                            />
-                            <label htmlFor="name">Email: </label>
-                            <input
-                                type="text"
-                                name="email"
-                                // placeholder='Email'
-                                className="form-control"
-                            />
-                            <label htmlFor="message">Message: </label>
-                            <textarea
-                                name="message"
-                                rows={5}
-                                // placeholder='message'
-                                className="form-control"
-                            ></textarea>
-                            <button type="submit" className="submit-btn btn">
-                                Send Message
-                            </button>
-                        </div>
-                    </form>
-                </article>
+            <SEO title='Get in Touch' descFor='contactPage' />
+            <section className='content-page gutter'>
+                <div className='content-page-inner'>
+                    <Header title='Get in Touch' />
+
+                    <article className='contact-form'>
+                        <form>
+                            <div className='form-group card no-hover no-placement'>
+                                <label htmlFor='name'>Name: </label>
+                                <input
+                                    type='text'
+                                    name='name'
+                                    // placeholder='name'
+                                    className='form-control'
+                                />
+                                <label htmlFor='name'>Email: </label>
+                                <input
+                                    type='text'
+                                    name='email'
+                                    // placeholder='Email'
+                                    className='form-control'
+                                />
+                                <label htmlFor='message'>Message: </label>
+                                <textarea
+                                    name='message'
+                                    rows={5}
+                                    // placeholder='message'
+                                    className='form-control'></textarea>
+                                <button type='submit' className='submit-btn btn center'>
+                                    Send Message
+                                </button>
+                            </div>
+                        </form>
+                    </article>
+                </div>
             </section>
         </Layout>
     );
