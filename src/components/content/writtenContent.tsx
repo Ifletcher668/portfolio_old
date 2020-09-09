e686be86b5faf71a7c000a1d9c1c970aa5b17e56
IFletcher <Isiahfletcher7@gmail.com>
2020-09-08 22:11:58 -0600

    refactoring

--- src/components/content/writtenContent.tsx
+++ src/components/content/writtenContent.tsx
@@ -1,35 +1,39 @@
-import React from 'react';
+import React, { ReactChildren } from 'react';
 import Blog from '../blog/blog';
 import Poem from '../poem/poem';
 import { Link } from 'gatsby';
 
-export const Blogs: React.FC<IWrittenContent> = (props: IWrittenContent) => {
-    const { blogs, poetry, title, showLink } = props;
+interface IWrittenContent extends IProps {
+    blogs?: Blog[];
+    poetry?: Poem[];
+    showLink: boolean;
+    showRecentPosts?: boolean;
+    children: React.ReactNode;
+}
 
-    const pastMonth = new Date().getMonth() - 1;
+const mapPoetry = (poetry: Poem[]) => {
+    return poetry.map((poem) => <Poem key={poem.title} {...poem} />);
+};
+
+const mapBlogs = (blogs: Blog[]) => {
+    return blogs.map((blog) => <Blog key={blog.title} {...blog} />);
+};
+
+export const Blogs: React.FC<IWrittenContent> = (props: IWrittenContent) => {
+    const { blogs, poetry, title, showLink, children } = props;
+
+    // const pastMonth = new Date().getMonth() - 1;
 
     const handleContentType = () => {
-        if (blogs && !poetry) return blogs.map((blog, idx) => <Blog key={idx} {...blog} />);
-        else if (poetry && !blogs) return poetry.map((poem, idx) => <Poem key={idx} {...poem} />);
-        else if (poetry && blogs) {
-            // all recent posts
-            return (
-                <>
-                    {blogs.map((blog, idx) => (
-                        // blog.createdAt >
-                        <Blog key={idx} {...blog} />
-                    ))}
-                    {poetry.map((poem, idx) => (
-                        <Poem key={idx} {...poem} />
-                    ))}
-                </>
-            );
-        }
+        if (blogs && !poetry) return mapBlogs(blogs);
+        else if (poetry && !blogs) return mapPoetry(poetry);
+        else if (poetry && blogs) return mapBlogs(blogs) && mapPoetry(poetry);
     };
 
     return (
         <section className=' section gutter'>
-            <h1>{title}</h1>
+            <h1>{title ? title : ''}</h1>
+            {children}
             <div className='section-center'>{handleContentType()}</div>
             {showLink && (
                 <Link to={`${blogs ? '/writing/blogs' : '/writing/poetry'}`} className='btn center-btn'>
