c19e617ce74ef86f8debf3462fe8eea07fbd24ed
Isiah Fletcher <Isiah@Colleen-Fletchers-MacBook.local>
2020-09-12 09:23:12 -0600

    added new image and query

--- src/graphql/queries/images.tsx
+++ src/graphql/queries/images.tsx
@@ -1,4 +1,4 @@
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from "gatsby";
 
 export const ImageFragment = graphql`
     fragment FluidImage on File {
@@ -10,10 +10,37 @@ export const ImageFragment = graphql`
     }
 `;
 
+// allow variable so there's only one hook
+// export const useCoverPhoto = (imgUrl: string) => {
+//     const ImageData = useStaticQuery(graphql`
+//         query {
+//             coverImage: file(relativePath: { eq: "cover-photo.png" }) {
+//                 ...FluidImage
+//             }
+//         }
+//     `);
+
+//     return ImageData;
+// };
+
+// hero image
 export const useCoverPhoto = () => {
     const ImageData = useStaticQuery(graphql`
         query {
             coverImage: file(relativePath: { eq: "cover-photo.png" }) {
+                ...FluidImage
+            }
+        }
+    `);
+
+    return ImageData;
+};
+
+// for base writing page
+export const useTeaPhoto = () => {
+    const ImageData = useStaticQuery(graphql`
+        query {
+            coverImage: file(relativePath: { eq: "oriento-unsplash.jpg" }) {
                 ...FluidImage
             }
         }
--- src/pages/writing/index.tsx
+++ src/pages/writing/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import Services from "../../components/services/services";
 import Layout from "../../components/layout/layout";
 import SEO from "../../components/SEO/SEO";
+import { useTeaPhoto } from "../../graphql/queries/images";
+import Image from "gatsby-image";
 
 export default (props: IProps) => {
+    const image = useTeaPhoto();
+
     return (
         <Layout>
             <SEO title="Writing Resources" descFor="writingPage" />
+            {/* <Image /> */}
             <Services type="writing" title={`My Writing`} />
         </Layout>
     );
