7c3a5f446a4e13b0a09587b9f8d5aed90fe3db74
IFletcher <Isiahfletcher7@gmail.com>
2020-09-11 22:39:11 -0600

    added netlify contact hook

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/layout/layout';
 import { Header } from '../components/titles/titles';
 import SEO from '../components/SEO/SEO';
+import ContactForm from '../components/contact_form/index';
 
 const contact: React.FC<IProps> = (props: IProps) => {
     return (
@@ -10,35 +11,7 @@ const contact: React.FC<IProps> = (props: IProps) => {
             <section className='content-page gutter'>
                 <div className='content-page-inner'>
                     <Header title='Get in Touch' center={false} value={1} />
-                    <article className='contact-form'>
-                        <form>
-                            <div className='form-group card no-hover no-placement'>
-                                <label htmlFor='name'>Name: </label>
-                                <input
-                                    type='text'
-                                    name='name'
-                                    // placeholder='name'
-                                    className='form-control'
-                                />
-                                <label htmlFor='name'>Email: </label>
-                                <input
-                                    type='text'
-                                    name='email'
-                                    // placeholder='Email'
-                                    className='form-control'
-                                />
-                                <label htmlFor='message'>Message: </label>
-                                <textarea
-                                    name='message'
-                                    rows={5}
-                                    // placeholder='message'
-                                    className='form-control'></textarea>
-                                <button type='submit' className='submit-btn btn center'>
-                                    Send Message
-                                </button>
-                            </div>
-                        </form>
-                    </article>
+                    <ContactForm />
                 </div>
             </section>
         </Layout>
