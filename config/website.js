e8241b230dfc4e4f40226296d14a728063aa0cdc
IFletcher <Isiahfletcher7@gmail.com>
2020-09-13 11:13:27 -0600

    added packages. updating configs

---
+++ config/website.js
@@ -0,0 +1,21 @@
+module.exports = {
+    siteTitle: 'Isiah Fletcher',
+    siteTitleAlt: 'Drummer | Poet | Software Engineer',
+    siteTitleShort: 'Isiahfletcher',
+    siteUrl: 'https://isiahfletcher.com',
+    lang: 'en',
+    pathPrefix: '/',
+    siteLogo: 'images/logo.png',
+    siteDescription:
+        'Drummer. Web Developer. Poet. Isiah Fletcher lives in Beautiful Boise, Idaho',
+    minibio:
+        'Isiah Fletcher is a Javascript engineer, and a professional drummer and writer. He write content for the web, builds websites, writes poetry, and tours with his bands, Aterrima and Weald and Woe.',
+    author: 'Isiah J. Fletcher',
+    organization: 'Isiah Fletcher',
+
+    // social media
+    facebook: 'https://www.facebook.com/fletchmydrumsticks',
+    instagram: 'https://www.instagram.com/fletchmydrumsticks/?hl=en',
+    github: 'https://github.com/Ifletcher668?tab=repositories',
+    linkedIn: 'https://www.linkedin.com/in/isiah-fletcher-074060159/',
+}
--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 interface ICardProps extends IProps {
     children: React.ReactChild;
     // placement: boolean;
