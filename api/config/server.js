9dd07a83cce54bc602874ba06c827640cfb65c4a
IFletcher <Isiahfletcher7@gmail.com>
2020-10-13 15:52:17 -0600

    api

--- api/config/server.js
+++ api/config/server.js
@@ -1,9 +1,9 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "bbe6024e62d6377e3c9e578386574eb3"),
+module.exports = ({env}) => ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    admin: {
+        auth: {
+            secret: env('ADMIN_JWT_SECRET', 'bbe6024e62d6377e3c9e578386574eb3'),
+        },
     },
-  },
-});
+})
